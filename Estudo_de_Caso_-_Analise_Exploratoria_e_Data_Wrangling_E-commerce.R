a8b7e24bbf95544a295617b13e608f0f56ecdc9e
EduardoBran <eduardo.ads1814@gmail.com>
2024-03-15 20:02:59 -0300

    analisando os dados - parte 1

--- Estudo_de_Caso_-_Analise_Exploratoria_e_Data_Wrangling_E-commerce.R
+++ Estudo_de_Caso_-_Analise_Exploratoria_e_Data_Wrangling_E-commerce.R
@@ -6,6 +6,14 @@ getwd()
 
 
 
+## Importando Pacotes
+library(readxl)         # carregar arquivos
+library(dplyr)          # manipula dados
+library(tidyr)          # manipula dados (funcao pivot_longer)
+library(ggplot2)        # gera gráficos
+
+
+
 
 #############################             Estudo de Caso             #############################
 
@@ -46,12 +54,208 @@ getwd()
 
 
 
-## Importando Pacotes
-library(readxl)         # carregar arquivos
-library(dplyr)          # manipula arquivos
+
+
+
+## Carregando os Dados
+df <- data.frame(read.csv2("dados/dataset.csv", sep = ","))
+head(df)
+
+
+## Realizando Análise Inicial (Sumário Estatístico, Veriricação de Valores NA, '' e especiais)
+
+analise_inicial <- function(dataframe_recebido) {  # para encotrar linhas com caracter especial, vá para o fim do script
+  # Sumário
+  print(dim(dataframe_recebido))
+  print(str(dataframe_recebido))
+  print(summary(dataframe_recebido))
+  cat("\n\n\n####  VERIFICANDO VALORES NA  ####\n\n")
+  valores_na <- colSums(is.na(dataframe_recebido))
+  if(any(valores_na > 0)) {
+    cat("\n-> Colunas com valores NA:\n\n")
+    print(valores_na[valores_na > 0])
+  } else {
+    cat("\n-> Não foram encontrados valores NA.\n")
+  }
+  cat("\n\n\n####  VERIFICANDO VALORES VAZIOS ''  ####\n\n")
+  valores_vazios <- sapply(dataframe_recebido, function(x) sum(x == ""))
+  if(any(valores_vazios > 0)) {
+    cat("\n-> Colunas com valores vazios \"\":\n\n")
+    print(valores_vazios[valores_vazios > 0])
+  } else {
+    cat("\n-> Não foram encontrados valores vazios \"\".\n")
+  }
+  cat("\n\n\n####  VERIFICANDO VALORES COM CARACTERES ESPECIAIS  ####\n\n")
+  caracteres_especiais <- sapply(dataframe_recebido, function(x) {
+    sum(sapply(x, function(y) {
+      if(is.character(y) && length(y) == 1) {
+        any(charToRaw(y) > 0x7E | charToRaw(y) < 0x20)
+      } else {
+        FALSE
+      }
+    }))
+  })
+  if(any(caracteres_especiais > 0)) {
+    cat("\n-> Colunas com caracteres especiais:\n\n")
+    print(caracteres_especiais[caracteres_especiais > 0])
+  } else {
+    cat("\n-> Não foram encontrados caracteres especiais.\n")
+  }
+}
+
+analise_inicial(df)
+
+
+## Modificando todas as variáveis do tipo chr para factor
+df <- dplyr::mutate_if(df, is.character, as.factor)
+str(df)
+
+
+## Dividindo o Dataframe em Variáveis Categoricas, Variáveis Numéricas e Variável Alvo
+names(df)
+
+# Dataframe somente com as variáveis categóricas
+df_categoricas <- df %>% 
+  select(where(is.factor))
+
+# Dataframe somente com as variáveis numéricas
+df_numericas <- df %>% 
+  select(where(is.numeric)) %>% 
+  select(-ID, -entregue_no_prazo)
+
+# Dataframe somente com a variável alvo/target "entregue_no_prazo"
+df_target <- df %>%
+  select(entregue_no_prazo) %>% 
+  mutate(entregue_no_prazo = factor(entregue_no_prazo))
+
+
+
+#### Explorando as Variáveis Numéricas
+
+## Resumo Estatístico
+summary(df_numericas)
+
+# As colunas numero_chamadas_cliente, avaliacao_cliente e custo_produto parecem ter uma distribuição bastante simétrica (média e mediana não são tão diferentes).
+# As colunas compras_anteriores e desconto parecem estar inclinadas para a direita (Média maior do que a Mediana).
+# A coluna peso_gramas parece estar mais inclinada para a esquerda (Média menor do que a Mediana).
+
+
+## Visualizando Através de Gráficos
+
+# Transformar o dataframe para o formato longo apropriado para o ggplot
+df_long_num <- df_numericas %>% 
+  pivot_longer(
+    cols = everything(), 
+    names_to = "variable", 
+    values_to = "value"
+  )
+
+
+# Box Plot
+ggplot(data = df_long_num, aes(x = variable, y = value, fill = variable)) + 
+  geom_boxplot(width = 0.25) +                                                       # Ajusta a largura dos boxes
+  scale_fill_manual(values = rep("magenta", length(unique(df_long_num$variable)))) + 
+  theme_minimal() + 
+  theme(legend.position = "none", 
+        axis.text.x = element_text(angle = 45, vjust = 0.5)) + 
+  labs(x = NULL, y = "Valores") + 
+  facet_wrap(~variable, scales = "free")
+
+# Histograma  com linha de densidade para cada variável
+ggplot(df_long_num, aes(x = value)) +
+  geom_histogram(aes(y = ..density..), binwidth = 1, colour = "black", fill = "gray") +
+  geom_density(alpha = .2, fill = "#FF6666") +
+  facet_wrap(~ variable, scales = "free") +
+  theme_minimal() +
+  labs(title = "Histograma com Linha de Densidade para Cada Variável",
+       x = "Valor",
+       y = "Densidade")
+
+rm(df_long_num)
+
+
+
+
+#### Explorando as Variáveis Categóricas
+
+## Resumo estatístico
+summary(df_categoricas)
+
+
+## Visualizando Através de Gráficos
+head(df_categoricas)
+
+# Transforma o dataframe de formato largo para longo
+df_long_cat <- df_categoricas %>% 
+  pivot_longer(cols = everything(), names_to = "categories", values_to = "values")
+
+
+library(patchwork)
+
+# Gráfico para corredor_armazem
+grafico_corredor <- ggplot(df_categoricas, aes(x = corredor_armazem)) +
+  geom_bar() +
+  labs(title = "Distribuição de corredor_armazem")
+
+# Gráfico para modo_envio
+grafico_modo_envio <- ggplot(df_categoricas, aes(x = modo_envio)) +
+  geom_bar() +
+  labs(title = "Distribuição de modo_envio")
+
+# Gráfico para prioridade_produto
+grafico_prioridade <- ggplot(df_categoricas, aes(x = prioridade_produto)) +
+  geom_bar() +
+  labs(title = "Distribuição de prioridade_produto")
+
+# Gráfico para genero
+grafico_genero <- ggplot(df_categoricas, aes(x = genero)) +
+  geom_bar() +
+  labs(title = "Distribuição de genero")
+
+# Colocando os gráficos em um único plot
+grafico_completo <- (grafico_corredor | grafico_modo_envio) / (grafico_prioridade | grafico_genero)
+
+grafico_completo
+
+rm(grafico_corredor, grafico_modo_envio, grafico_prioridade, grafico_genero)
+
+
+
+
+
+## Explorando a Variável Alvo
+
+# Resumo Estatístico
+summary(df_target)
+
+# Visualizando Através de Gráficos
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+# Alternativa para encontrar a linha com caractere especial (somente 1 linha)
+for (i in 1:nrow(df2)) {
+  if (any(charToRaw(df2$installment[i]) > 0x7E) | any(charToRaw(df2$installment[i]) < 0x20)) {
+    cat("A linha com o caractere especial na coluna 'installment' é:", i, "\n")
+    break
+  }
+}
+# Encontrar linhas QUE NÃO TEM caracteres especiais na coluna ESPECÍFICA (para mais de 1 linha com caracteres especiais)
+linhas_com_caracteres_especiais <- which(sapply(df2$int_rate, function(x) grepl("[^\\x20-\\x7E]", x)))
+
+# Exibir as linhas que contêm caracteres especiais, se houver
+if (length(linhas_com_caracteres_especiais) > 0) {
+  cat("Foram encontrados valores com caracteres especiais nas linhas:", paste(linhas_com_caracteres_especiais, collapse=", "), "\n")
+} else {
+  cat("Não foram encontrados valores com caracteres especiais.\n")
+}
